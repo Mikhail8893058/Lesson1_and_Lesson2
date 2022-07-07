9cf2375876305b2c8ff501151fa639a93b6d0625
Mikhail <mishadyshlevich@mail.ru>
2022-07-07 13:00:54 +0300

    add 02

---
+++ 02/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max; 
+
+if (b < a && a > c)
+{
+    max = a;
+}
+else if (a < b && b > c)
+{
+    max = b;
+}
+else
+{
+    max = c;
+}
+Console.WriteLine("max = " + max);
