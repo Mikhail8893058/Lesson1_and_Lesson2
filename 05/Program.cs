7f8e74edc08c854ca25ed7cde63097704b9736de
Mikhail <mishadyshlevich@mail.ru>
2022-07-07 13:01:55 +0300

    05

---
+++ 05/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string str = Convert.ToString(number);
+
+if ( number > 99 && number < 1000 )
+{
+    Console.WriteLine(str[1]);
+}
+else
+{
+    Console.WriteLine("не трехзначное число");
+}
