223f163078d7af17cd8d9c03a04870636f423d95
Mikhail <mishadyshlevich@mail.ru>
2022-07-07 13:01:38 +0300

    04

---
+++ 04/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите любое число: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+int min = 2;
+
+while (min < numN)
+{
+    Console.Write(min + ", ");
+    min = min + 2;
+}
+Console.WriteLine(min);
+
+
