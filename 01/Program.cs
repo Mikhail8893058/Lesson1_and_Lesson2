9a45afd6084c7fc186a80ac72cd0fbe0eb4de899
Mikhail <mishadyshlevich@mail.ru>
2022-07-07 13:00:31 +0300

    add 01

---
+++ 01/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите number1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите number2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int max;
+int min;
+
+
+if (number1 > number2)
+{
+   max = number1;
+   min = number2;   
+}
+else
+{
+   max = number2;
+   min = number1;  
+}
+Console.WriteLine("max = " + max);
+Console.WriteLine("min = " + min);
