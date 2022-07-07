1eccb9af709b05800179abb0b4c64ab9358050cf
Mikhail <mishadyshlevich@mail.ru>
2022-07-07 13:02:06 +0300

    06

---
+++ 06/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string str = Convert.ToString(number);
+
+if ( number > 99)
+{
+    Console.Write("третья цифру заданного числа = ");
+    Console.Write(str[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
